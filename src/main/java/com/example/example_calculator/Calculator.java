e77ce5cd4a4dadb34e5872c959b2cf3ce7475e76
colale <colale1998@gmai.com>
2023-02-14 11:20:49 +0100

    new commit

--- src/main/java/com/example/example_calculator/Calculator.java
+++ src/main/java/com/example/example_calculator/Calculator.java
@@ -1,23 +1,80 @@
 package com.example.example_calculator;
+import java.util.Scanner;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Math.log;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class Calculator {
+    static double firstOperand;
+    static double secondOperand;
+    static boolean validInput = FALSE;
 
     public static void main(String[] args) {
-        launch();
+        Scanner scanner = new Scanner(System.in);
+        while (!validInput) {
+            try{
+                System.out.println("Insert Number 1:");
+                firstOperand = scanner.nextDouble();}
+            catch(Exception ex){validInput=FALSE;
+                scanner.nextLine();
+                continue;}
+            System.out.println("Select Operation:\n1)+\n2)-\n3)*\n4)/\n5)ln");
+            int num = scanner.nextInt();
+            switch (num) {
+                case 1:// +
+                    try{
+                        System.out.println("Insert Number 2:");
+                        secondOperand = scanner.nextDouble();
+                        System.out.println(firstOperand + secondOperand);
+                        break;}
+                    catch(Exception ex){validInput=FALSE;
+                        System.out.println("Input Error");
+                        scanner.nextLine();
+                        break;}
+                case 2:// -
+                    try{
+                        System.out.println("Insert Number 2");
+                        secondOperand = scanner.nextDouble();
+                        System.out.println(firstOperand - secondOperand);
+                        break;}
+                    catch(Exception ex){validInput=FALSE;
+                        System.out.println("Input Error");
+                        scanner.nextLine();
+                        break;}
+                case 3:// *
+                    try{
+                        System.out.println("Insert Number 2:");
+                        secondOperand = scanner.nextDouble();
+                        System.out.println(firstOperand * secondOperand);
+                        break;}
+                    catch(Exception ex){validInput=FALSE;
+                        System.out.println("Input Error");
+                        scanner.nextLine();
+                        break;}
+                case 4:// /
+                    try {
+                        System.out.println("Insert Number 2:");
+                        secondOperand = scanner.nextDouble();
+                        System.out.println(firstOperand / secondOperand);
+                        break;
+                    }
+                    catch(Exception ex){validInput=FALSE;
+                        System.out.println("Input Error:");
+                        scanner.nextLine();
+                    }
+                case 5:// /
+                    try{
+                        System.out.println(log(firstOperand));
+                        break;}
+                    catch(Exception ex){validInput=FALSE;
+                        System.out.println("Input Error");
+
+                        scanner.nextLine();
+                        break;}
+                default:
+                    System.out.println("Invalid number.Retry");
+                    scanner.nextLine();
+            }
+        }
     }
 }
